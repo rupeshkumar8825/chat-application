e165d17c9cb4eccf604e01d24ca1c7877322039e
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2023-02-25 15:57:04 +0530

    Added the event to listen to message coming from client

--- server/index.js
+++ server/index.js
@@ -20,6 +20,14 @@ const webSocketServer = new WebSocket.Server({
 webSocketServer.on('connection', function(ws){
     ws.send("Hello from the server!! your connection request has been approved");
     console.log("The details of the client is as follows \n", ws);
+
+    // now here we can keep on waiting for the message event 
+    // we will define the new event for receiving the message 
+    ws.on('message', (data)=>{
+        console.log("The message from the client side is as follows \n\n", data.toString());
+
+        // say everything went fine 
+    })
     // say everything went fine 
 })
 
