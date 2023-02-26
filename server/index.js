628919559109086a589dc3560cb90cca32a6d3d8
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2023-02-26 10:42:55 +0530

    Added the code to verify the client

--- server/index.js
+++ server/index.js
@@ -4,12 +4,26 @@ console.log("Hi this is nodejs server");
 const express = require("express")
 const app = express();
 const WebSocket = require('ws')
+const path = require('path')
 const PORT = 8567;
 
+// serving the html files by the server
+// we will use this on root for this purpose 
+app.use("/", express.static(path.resolve(__dirname, '../client/')))
+
+
+// here we can add the callback function 
+const server = app.listen(9876)
 
 // we have to define the websocket server
 const webSocketServer = new WebSocket.Server({
-    port : PORT
+    // port : PORT
+    // server: server
+    noServer : true
+
+    // we can use verifyclient in websockets to verify the user or client 
+    // this helps in checking whether we want to connect with the client or not 
+
 }, ()=>{
     console.log("Web socket server is running\n")
 })
@@ -41,3 +55,27 @@ webSocketServer.on('connection', function(ws){
     // say everything went fine 
 })
 
+
+
+
+// this end point will hit whenever the new client wants to connect 
+// then this server will try to upgrade 
+// this shows that this connection can be upgraded hence we have defined the following one 
+server.on('upgrade', async function upgrade(request, socket, head)
+{
+    // do what you normally you do in verifyclient() here and then use the
+    // handleupgrade method to call the web socket server to establish the connection with this particular client for this purpose 
+
+    //test for authentication 
+
+
+    // once the authentication is done then we will call the handleupgrade function to upgrade to web scoket server and once the upgradation is complete then we will emit the connection event on the callback for this purpose 
+    webSocketServer.handleUpgrade(request, socket, head, function done(ws){
+
+        // here we are emitting the connection event to connect to the web socket server 
+        webSocketServer.emit('connection', ws, request, [])
+    })
+
+    // say everything went fine 
+    return true;
+})
